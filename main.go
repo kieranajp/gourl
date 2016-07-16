bf82c3b9345fa6c57d0861762dbd1b408172ef2d
Kieran Patel <me@kieranajp.co.uk>
2016-07-17 01:30:06 +0200

    URL shortener implementation

--- main.go
+++ main.go
@@ -1,24 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"github.com/kataras/iris"
-	"gopkg.in/redis.v3"
+	"gopkg.in/redis.v4"
 )
 
-func initRedis() *redis.Client {
-	return redis.NewClient(&redis.Options{
-		Addr: "redis",
+var redisStorage = redis.NewClient(&redis.Options{
+	Addr: "redis:6379",
+})
+
+type UrlAPI struct {
+	*iris.Context
+}
+
+func (u UrlAPI) Get() {
+	u.Write("Hello world")
+}
+
+func (u UrlAPI) GetBy(id string) {
+	url, err := redisStorage.Get(fmt.Sprintf("URL_%s", id)).Result()
+
+	if err != nil {
+		u.JSON(iris.StatusNotFound, iris.Map{
+			"error": fmt.Sprintf("Not found: %s", id),
+		})
+		return
+	}
+
+	u.JSON(iris.StatusOK, iris.Map{
+		"location": url,
 	})
 }
 
+func (u UrlAPI) Post() {
+	url := u.FormValue("url")
+	key := getNextKey()
+
+	err := redisStorage.Set(fmt.Sprintf("URL_%d", key), url, 0).Err()
+
+	if err != nil {
+		u.JSON(iris.StatusInternalServerError, iris.Map{
+			"error": "Failed to save",
+		})
+	}
+
+	u.JSON(iris.StatusCreated, iris.Map{
+		"location": url,
+		"key":      key,
+	})
+}
+
+func getNextKey() int64 {
+	if err := redisStorage.Incr("key").Err(); err != nil {
+		panic(err)
+	}
+
+	key, _ := redisStorage.Get("key").Int64()
+
+	return key
+}
+
 func main() {
-	redisStorage := initRedis()
 	defer redisStorage.Close()
 
-	iris.Get("/", func(c *iris.Context) {
-		c.JSON(iris.StatusOK, iris.Map{
-			"Hello": "World",
-		})
-	})
+	iris.API("/", UrlAPI{})
 	iris.Listen("0.0.0.0:3001")
 }
